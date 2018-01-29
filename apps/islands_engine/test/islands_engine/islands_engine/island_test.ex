0b102e405eca5ad1b63d3b066807d82d905e4d47
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-01-28 20:03:37 -0500

    Adds ability to create the other type of islands.

--- apps/islands_engine/test/islands_engine/islands_engine/island_test.ex
+++ apps/islands_engine/test/islands_engine/islands_engine/island_test.ex
@@ -10,6 +10,7 @@ defmodule IslandsEngine.IslandTest do
   test "can create a square island" do
     {:ok, upper_left} = Coordinate.new(1, 1)
     {:ok, %Island{coordinates: coordinates}} = Island.new(:square, upper_left)
+
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 1})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 2})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 1})
@@ -19,12 +20,39 @@ defmodule IslandsEngine.IslandTest do
   test "can create an atoll island" do
     {:ok, upper_left} = Coordinate.new(1, 1)
     {:ok, %Island{coordinates: coordinates}} = Island.new(:atoll, upper_left)
-    IO.inspect(coordinates)
+
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 1})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 3})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 1})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 2})
     assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 3})
+  end
+
+  test "can create a dot island" do
+    {:ok, upper_left} = Coordinate.new(1, 1)
+    {:ok, %Island{coordinates: coordinates}} = Island.new(:dot, upper_left)
+
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 1})
+  end
+
+  test "can create an l-shape island" do
+    {:ok, upper_left} = Coordinate.new(1, 1)
+    {:ok, %Island{coordinates: coordinates}} = Island.new(:l_shape, upper_left)
+
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 1})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 2})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 3})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 3})
+  end
+
+  test "can create an s-shape island" do
+    {:ok, upper_left} = Coordinate.new(1, 1)
+    {:ok, %Island{coordinates: coordinates}} = Island.new(:s_shape, upper_left)
+
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 1, row: 2})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 1})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 2, row: 2})
+    assert MapSet.member?(coordinates, %IslandsEngine.Coordinate{col: 3, row: 1})
   end
 
   test "handles invalid island types" do

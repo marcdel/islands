12aa6e92285e155a9fa03f3495c6b87bd8f4e67a
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-01-29 20:00:46 -0500

    Adds the ability to add hit and miss guesses

--- apps/islands_engine/lib/islands_engine/islands_engine/guesses.ex
+++ apps/islands_engine/lib/islands_engine/islands_engine/guesses.ex
@@ -3,12 +3,20 @@ defmodule IslandsEngine.Guesses do
   Holds the list of guesses on the opponent's board
   """
 
-  alias __MODULE__
+  alias IslandsEngine.{Guesses, Coordinate}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new do
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+  end
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
   end
 end
--- apps/islands_engine/test/islands_engine/islands_engine/guesses_test.ex
+++ apps/islands_engine/test/islands_engine/islands_engine/guesses_test.ex
@@ -4,11 +4,25 @@ defmodule IslandsEngine.GuessesTest do
   use ExUnit.Case
   doctest IslandsEngine.Guesses
 
-  alias IslandsEngine.Guesses
+  alias IslandsEngine.{Guesses, Coordinate}
 
   test "can create new list of guesses" do
     %Guesses{hits: hits, misses: misses} = Guesses.new()
     assert MapSet.size(hits) == 0
     assert MapSet.size(misses) == 0
+  end
+
+  test "can add hits" do
+    guesses = Guesses.new()
+    {:ok, hit} = Coordinate.new(1, 1)
+    %Guesses{hits: hits} = Guesses.add(guesses, :hit, hit)
+    assert hit in hits
+  end
+
+  test "can add misses" do
+    guesses = Guesses.new()
+    {:ok, miss} = Coordinate.new(1, 1)
+    %Guesses{misses: misses} = Guesses.add(guesses, :miss, miss)
+    assert miss in misses
   end
 end

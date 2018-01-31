cb950189bc36ff5e6f589363246c1d765ecd8b5d
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-01-30 20:04:19 -0500

    Adds ability to check whether all islands have been positioned

--- apps/islands_engine/lib/islands_engine/islands_engine/board.ex
+++ apps/islands_engine/lib/islands_engine/islands_engine/board.ex
@@ -7,11 +7,15 @@ defmodule IslandsEngine.Board do
 
   def new, do: %{}
 
-  def position_island(board, key, %Island{} = island) do
+  def position_island(%{} = board, key, %Island{} = island) do
     case overlaps_existing_island?(board, key, island) do
       true -> {:error, :overlapping_island}
       false -> Map.put(board, key, island)
     end
+  end
+
+  def all_islands_positioned?(%{} = board) do
+    Enum.all?(Island.types(), &(Map.has_key?(board, &1)))
   end
 
   defp overlaps_existing_island?(board, new_key, new_island) do

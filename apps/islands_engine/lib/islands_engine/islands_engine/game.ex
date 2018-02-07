2cca72f2bed2f233cc770e37f42af203217bf242
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-02-06 21:04:55 -0500

    Adds ability for players to position islands

--- apps/islands_engine/lib/islands_engine/islands_engine/game.ex
+++ apps/islands_engine/lib/islands_engine/islands_engine/game.ex
@@ -1,7 +1,11 @@
 defmodule Game do
+  @moduledoc false
+
   use GenServer
 
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Island, Guesses, Coordinate, Rules}
+
+  @players [:player_one, :player_two]
 
   def start_link(player_name) when is_binary(player_name) do
     GenServer.start_link(__MODULE__, player_name, [])
@@ -18,6 +22,10 @@ defmodule Game do
     GenServer.call(game, {:add_player, player_name})
   end
 
+  def position_island(game, player, key, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, key, row, col})
+  end
+
   def handle_call({:add_player, name}, _from, game_state) do
     with {:ok, rules} <- Rules.check(game_state.rules, :add_player) do
       game_state
@@ -29,11 +37,35 @@ defmodule Game do
     end
   end
 
+  def handle_call({:position_island, player, key, row, col}, _from, game_state) do
+    board = player_board(game_state, player)
+
+    with {:ok, rules} <- Rules.check(game_state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(key, coordinate),
+         %{} = board <- Board.position_island(board, key, island) do
+      game_state
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, game_state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, game_state}
+      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, game_state}
+    end
+  end
+
+  defp player_board(game_state, player), do: Map.get(game_state, player).board
+
   defp set_player_two_name(game_state, player_name) do
     put_in(game_state.player_two.name, player_name)
   end
 
   defp update_rules(game_state, rules), do: %{game_state | rules: rules}
+
+  defp update_board(game_state, player, board) do
+    Map.update!(game_state, player, fn player -> %{player | board: board} end)
+  end
 
   defp reply_success(game_state, reply), do: {:reply, reply, game_state}
 end
--- apps/islands_engine/test/islands_engine/islands_engine/game_test.exs
+++ apps/islands_engine/test/islands_engine/islands_engine/game_test.exs
@@ -3,14 +3,22 @@ defmodule GameTest do
 
   use ExUnit.Case
 
-  alias IslandsEngine.{Guesses, Rules}
+  alias IslandsEngine.{Board, Island, Guesses, Coordinate, Rules}
 
   test "can start a new game with a player name" do
     {:ok, game} = Game.start_link("Marc")
 
     %{
-      player_one: %{board: %{}, guesses: %Guesses{}, name: "Marc"},
-      player_two: %{board: %{}, guesses: %Guesses{}, name: nil},
+      player_one: %{
+        board: %{},
+        guesses: %Guesses{},
+        name: "Marc"
+      },
+      player_two: %{
+        board: %{},
+        guesses: %Guesses{},
+        name: nil
+      },
       rules: %Rules{
         player_one: :islands_not_set,
         player_two: :islands_not_set,
@@ -24,8 +32,53 @@ defmodule GameTest do
     :ok = Game.add_player(game, "Jackie")
 
     %{
-      player_two: %{name: "Jackie"},
-      rules: %Rules{state: :players_set}
+      player_two: %{
+        name: "Jackie"
+      },
+      rules: %Rules{
+        state: :players_set
+      }
     } = :sys.get_state(game)
+  end
+
+  describe "position_island/5" do
+    setup do
+      {:ok, game} = Game.start_link("Marc")
+      :ok = Game.add_player(game, "Jackie")
+
+      [game: game]
+    end
+
+    test "players can position islands", %{game: game} do
+      :ok = Game.position_island(game, :player_one, :dot, 1, 1)
+      :ok = Game.position_island(game, :player_two, :square, 1, 1)
+
+      game_state = :sys.get_state(game)
+      assert %Island{} = game_state.player_one.board.dot
+      assert %Island{} = game_state.player_two.board.square
+    end
+
+    test "handles invalid coordinates", %{game: game} do
+      assert {:error, :invalid_coordinate} = Game.position_island(game, :player_one, :dot, 12, 12)
+    end
+
+    test "handles invalid island types", %{game: game} do
+      assert {:error, :invalid_island_type} =
+               Game.position_island(game, :player_one, :invalid_island, 1, 1)
+    end
+
+    test "handles invalid island positions", %{game: game} do
+      assert {:error, :invalid_coordinate} =
+               Game.position_island(game, :player_one, :l_shape, 10, 10)
+    end
+
+    test "handles rule violations", %{game: game} do
+      # We shouldn't be able to position islands when it's a player's turn.
+      :sys.replace_state(game, fn game_state ->
+        %{game_state | rules: %Rules{state: :player_one_turn}}
+      end)
+
+      assert :error = Game.position_island(game, :player_one, :dot, 1, 1)
+    end
   end
 end

435d2fde1fa934ca9b4f401b5bdb4ca043050968
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-02-12 20:59:35 -0500

    Adds ability to easily start and stop games from supervisor.

--- apps/islands_engine/lib/islands_engine/islands_engine/game_supervisor.ex
+++ apps/islands_engine/lib/islands_engine/islands_engine/game_supervisor.ex
@@ -10,4 +10,18 @@ defmodule IslandsEngine.GameSupervisor do
   def init(:ok) do
     Supervisor.init([Game], strategy: :simple_one_for_one)
   end
+
+  def start_game(player_name) do
+    Supervisor.start_child(__MODULE__, [player_name])
+  end
+
+  def stop_game(player_name) do
+    Supervisor.terminate_child(__MODULE__, pid_from_name(player_name))
+  end
+
+  defp pid_from_name(player_name) do
+    player_name
+    |> Game.via_tuple()
+    |> GenServer.whereis()
+  end
 end
--- apps/islands_engine/test/islands_engine/islands_engine/game_supervisor_test.exs
+++ apps/islands_engine/test/islands_engine/islands_engine/game_supervisor_test.exs
@@ -1,9 +1,43 @@
 defmodule IslandsEngine.GameSupervisorTest do
   use ExUnit.Case
 
-  alias IslandsEngine.GameSupervisor
+  alias IslandsEngine.{GameSupervisor, Game, Guesses, Rules}
 
-  test "can start a new GameSupervisor" do
-    assert {:ok, pid} = GameSupervisor.start_link(:bleh)
+  test "supervisor can start new games with a player name" do
+    {:ok, game} = GameSupervisor.start_game("Player 1")
+
+    %{
+      player_one: %{
+        board: %{},
+        guesses: %Guesses{},
+        name: "Player 1"
+      },
+      player_two: %{
+        board: %{},
+        guesses: %Guesses{},
+        name: nil
+      },
+      rules: %Rules{
+        player_one: :islands_not_set,
+        player_two: :islands_not_set,
+        state: :initialized
+      }
+    } = :sys.get_state(game)
+
+    GameSupervisor.stop_game("Player 1")
+  end
+
+  test "can stop existing games" do
+    {:ok, game} = GameSupervisor.start_game("Player 1")
+    assert Process.alive?(game)
+
+    assert :ok = GameSupervisor.stop_game("Player 1")
+    refute Process.alive?(game)
+
+    assert Game.via_tuple("Player 1") |> GenServer.whereis() == nil
+  end
+
+  test "cannot stop non-existent games" do
+    assert {:error, _} = GameSupervisor.stop_game("Made up game")
   end
 end
